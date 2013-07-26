049b822d9c0b56ec705351af05b56c15ff4dd10f
Łukasz Korecki <lukasz@coffeesounds.com>
2013-07-26 12:11:02 +0100

    remove old hook url, and use a new

--- app.rb
+++ app.rb
@@ -27,6 +27,11 @@ class App < Sinatra::Base
   end
 
   post "/callback" do
+    status 410
+  end
+
+  post "/webhook" do
+    # TODO document this
     if ENV['UNREGISTER']
       status 410
       body "bye"

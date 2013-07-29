eaf72e2528cc5b9055cdb396bfa1dc29ffedbc06
Łukasz Korecki <lukasz@coffeesounds.com>
2013-07-29 09:26:28 +0100

    Cleanup and such

---
+++ scripts/dev_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export FLOWDOCK_TOKEN=<your room token>
+export TRELLO_DEVELOPER_API_KEY=<your trello key>
+
+bundle exec rackup
--- test/flowdock_poster_test.rb
+++ test/flowdock_poster_test.rb
@@ -1,8 +1,8 @@
+# encoding: utf-8
 require "minitest/autorun"
-require 'mocha/setup'
+require_relative "../lib/trello_event"
 require_relative "../lib/flowdock_poster"
 
 class FlowDockPosterTest < Minitest::Unit::TestCase
-
   # TODO
 end
